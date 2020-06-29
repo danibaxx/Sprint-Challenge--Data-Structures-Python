452b89b0f63e1b303694fe4402c10f8561ae9550
Danielle Baxter <danibaxx@hotmail.com>
2020-06-28 19:34:43 -0600

    attempted ring_buffer, tests failing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,31 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.value = []
 
     def append(self, item):
-        pass
+        # adds given element to the buffer
+        self.capacity += 1
+        self.value.append(item)
 
     def get(self):
-        pass
+        # returns all elements in the buffer in given order
+        # should not return none
+        if self.capacity == None:
+            return False
+        else:
+            return self.capacity
+
+buffer = RingBuffer(5)
+
+buffer.get()
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+buffer.append('d')
+buffer.append('e')
+
+buffer.get()
+
+buffer.append('f')
